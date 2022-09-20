f7686d2f5fd01e274d10bfc4f8d66788a80b7450
tmdqls2257 <tmdqls2257@gmail.com>
2022-09-20 23:05:49 +0900

    make it better

--- dist/src/users/dtos/user-register.dto.d.ts
+++ dist/src/users/dtos/user-register.dto.d.ts
@@ -1,5 +1,5 @@
 import { UserEntity } from '../users.entity';
-declare const UserRegisterDTO_base: import("@nestjs/common").Type<Pick<UserEntity, "username" | "email">>;
+declare const UserRegisterDTO_base: import("@nestjs/common").Type<Pick<UserEntity, "email" | "username">>;
 export declare class UserRegisterDTO extends UserRegisterDTO_base {
     password: string;
 }
